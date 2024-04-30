03157beb07154e3fb14e1dd4287dc3c72df49d38
fubbernuckin <fubbernuckin@gmail.com>
2024-04-29 23:29:14 -0500

    added multithreading, I believe the program is done. I do not have the time to implement thread safety unfortunately

--- automatic.h
+++ automatic.h
@@ -6,14 +6,23 @@
 #include <random>
 #include <ctime>
 #include <chrono>
+#include <vector>
 
 #include "banker.h"
 
 using namespace std;
 
+void request(banker* bank, int process, int resource, int amount) {
+    bank->addRequest(process, resource, amount);
+}
+void release(banker* bank, int process, int resource, int amount) {
+    bank->releaseRequest(process, resource, amount);
+}
+
 class automatic
 {
 private:
+    
     
 public:
     automatic();
@@ -23,11 +32,20 @@ public:
         time_t ct = time(nullptr);
         default_random_engine generator(ct);
         uniform_int_distribution distribution(0, numResources - 1);
+        
+        //create threads
+        vector<thread> threads;
         for (int i = 0; i < numProcesses; i++) {
             for (int j = 0; j < numRequests; j++) {
-                bank->addRequest(i, distribution(generator), 1);
-                bank->releaseRequest(i, distribution(generator), 1);
+                threads.push_back(thread(request, bank, i, distribution(generator), 1));
+                threads.push_back(thread(release, bank, i, distribution(generator), 1));
+                //bank->addRequest(i, distribution(generator), 1);
+                //bank->releaseRequest(i, distribution(generator), 1);
             }
+        }
+        
+        for(int i = 0; i < threads.size(); i++) {
+            threads[i].join();
         }
     }
 };
